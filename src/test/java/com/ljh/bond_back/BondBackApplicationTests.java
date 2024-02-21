804d176c3228b060929f3911c10af4534ffefc62
akdl911215 <akdl911215@naver.com>
2024-02-21 16:54:52 +0900

    bond back setting

---
+++ src/test/java/com/ljh/bond_back/BondBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljh.bond_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BondBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
